7f776a7755cf3e8b0c91bca32078fdd38eb2d97b
ByungSun Song <46879319+SBSun@users.noreply.github.com>
2024-06-17 23:48:03 +0900

    feat: 요청/파일 크기 예외 핸들링 추가

--- src/main/java/com/sun/fintrack/common/exception/handler/RestApiExceptionHandler.java
+++ src/main/java/com/sun/fintrack/common/exception/handler/RestApiExceptionHandler.java
@@ -3,8 +3,10 @@ package com.sun.fintrack.common.exception.handler;
 import com.sun.fintrack.common.exception.ValidationException;
 import com.sun.fintrack.common.response.ErrorResponse;
 
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
 import org.springframework.context.MessageSource;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,6 +25,16 @@ import lombok.extern.slf4j.Slf4j;
 public class RestApiExceptionHandler extends ResponseEntityExceptionHandler {
 
   private final MessageSource messageSource;
+
+  /**
+   * 전체 요청 크기 초과 예외 처리
+   */
+  @ExceptionHandler(SizeLimitExceededException.class)
+  public ResponseEntity<?> handleMaxRequestSizeException(SizeLimitExceededException e) {
+    log.info("Exception: " + e.getClass().getSimpleName() + "(" + e.getLocalizedMessage() + ")");
+    return ResponseEntity.ok(
+        new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "요청 크기가 너무 큽니다. 요청 크기는 10MB 이하로 제한됩니다."));
+  }
 
   @ExceptionHandler(ValidationException.class)
   public ResponseEntity<?> handleValidationException(ValidationException e, WebRequest request) {
