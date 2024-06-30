49a8cdade10a4ba439fc9bd0b9b7e0f1da699e33
ByungSun Song <46879319+SBSun@users.noreply.github.com>
2024-06-30 11:54:26 +0900

    feat: actuator와 prometheus 설정 추가 (#38)

--- src/main/java/com/sun/fintrack/FintrackApplication.java
+++ src/main/java/com/sun/fintrack/FintrackApplication.java
@@ -1,8 +1,10 @@
 package com.sun.fintrack;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -15,4 +17,8 @@ public class FintrackApplication {
     SpringApplication.run(FintrackApplication.class, args);
   }
 
+  @Bean
+  public InMemoryHttpExchangeRepository httpExchangeRepository() {
+    return new InMemoryHttpExchangeRepository();
+  }
 }
